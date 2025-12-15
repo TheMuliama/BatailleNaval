a8804850f7e0f8c99a17ce69016508dff336e585
Muliama <themuliama@proton.me>
2025-12-15 10:26:24 +0400

    Mise en place de CMake

---
+++ build/CMakeFiles/Serveur.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Serveur.dir/src/serveur.c.o"
+  "CMakeFiles/Serveur.dir/src/serveur.c.o.d"
+  "Serveur"
+  "Serveur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Serveur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
